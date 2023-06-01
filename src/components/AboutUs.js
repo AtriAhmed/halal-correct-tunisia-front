35846c2c3de6d6a9cc087d0f4acc8e97928f0e5f
Ahmed Atri <atriahmed.1999@gmail.com>
2023-06-01 12:19:00 +0100

    about card animation

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -4,39 +4,45 @@ import serviceIcon01 from "../images/service-icon-01.png"
 import serviceIcon02 from "../images/service-icon-02.png"
 import serviceIcon03 from "../images/service-icon-03.png"
 import serviceIcon04 from "../images/service-icon-04.png"
-import { Link } from "gatsby"
+import serviceIconHover01 from "../images/service-icon-hover-01.png"
+import serviceIconHover02 from "../images/service-icon-hover-02.png"
+import serviceIconHover03 from "../images/service-icon-hover-03.png"
+import serviceIconHover04 from "../images/service-icon-hover-04.png"
+import AboutCard from "./NewHomePage/About/AboutCard"
+
+const data = [
+  {
+    img: serviceIcon01,
+    hoverImg: serviceIconHover01,
+    title: "Valeurs Fondamentales",
+    content: "Engagement envers principes clés : Expertise, Orientation, Intégrité, Amélioration, Confiance."
+  },
+  {
+    img: serviceIcon02,
+    hoverImg: serviceIconHover02,
+    title: "Notre Mission",
+    content: "Halal QMS Center est la solution pour les industriels, les abattoirs, les restaurants et les hôtels qui font face à une transition vers le Halal"
+  },
+  {
+    img: serviceIcon03,
+    hoverImg: serviceIconHover03,
+    title: "Notre Equipe",
+    content: "Notre équipe d'experts en gestion de la qualité Halal. Accompagnement personnalisé, expérience éprouvée."
+  },
+  {
+    img: serviceIcon04,
+    hoverImg: serviceIconHover04,
+    title: "Halal QMS Center",
+    content: "Vous guide vers la certification Halal , pour assurer le maintien de votre certificat avec le respect permanent des lois et des normes nationales et internationales."
+  },
+]
 
 export default function AboutUs() {
   return (
     <div className="w-full max-w-7xl mx-auto p-10">
       <FadeUpOnScroll>
         <div className="grid grid-cols-12 gap-4">
-          <div className="col-span-3 shadow-xl p-7 flex flex-col gap-4 justify-between">
-            <div><img src={serviceIcon01} /></div>
-            <div className="font-bold text-xl">Valeurs Fondamentales</div>
-            <div className="text-sm">Engagement envers principes clés : Expertise, Orientation, Intégrité, Amélioration, Confiance.</div>
-            <div className=""><Link to="/about" className="text-[#0F70B7] underline" >En savoir plus</Link></div>
-          </div>
-          <div className="col-span-3 shadow-xl p-7 flex flex-col gap-4 justify-between">
-            <div><img src={serviceIcon02} /></div>
-            <div className="font-bold text-xl">Notre Mission</div>
-
-            <div className="text-sm">Halal QMS Center est la solution pour les industriels, les abattoirs, les restaurants et les hôtels qui font face à une transition vers le Halal</div>
-            <div className=""><Link to="/about" className="text-[#0F70B7] underline" >En savoir plus</Link></div>
-          </div>
-          <div className="col-span-3 shadow-xl p-7 flex flex-col gap-4 justify-between">
-            <div><img src={serviceIcon03} /></div>
-            <div className="font-bold text-xl">Notre Equipe</div>
-
-            <div className="text-sm">Notre équipe d'experts en gestion de la qualité Halal. Accompagnement personnalisé, expérience éprouvée.</div>
-            <div className=""><Link to="/about" className="text-[#0F70B7] underline" >En savoir plus</Link></div>
-          </div>
-          <div className="col-span-3 shadow-xl p-7 flex flex-col gap-4 justify-between">
-            <div><img src={serviceIcon04} /></div>
-            <div className="font-bold text-xl">Halal QMS Center</div>
-            <div className="text-sm">Vous guide vers la certification Halal , pour assurer le maintien de votre certificat avec le respect permanent des lois et des normes nationales et internationales.</div>
-            <div className=""><Link to="/about" className="text-[#0F70B7] underline" >En savoir plus</Link></div>
-          </div>
+          {data.map(card => <AboutCard img={card.img} hoverImg={card.hoverImg} title={card.title} content={card.content} />)}
         </div>
       </FadeUpOnScroll>
     </div>
---
+++ src/components/NewHomePage/About/AboutCard.js
@@ -0,0 +1,19 @@
+import { Link } from 'gatsby'
+import React, { useEffect, useState } from 'react'
+
+export default function AboutCard({ img, hoverImg, title, content }) {
+    const [hovered, setHovered] = useState(false)
+
+    useEffect(() => {
+        console.log(hovered)
+    }, [hovered])
+
+    return (
+        <div className="col-span-3 shadow-xl p-7 flex flex-col gap-4 justify-between hover:bg-cover hover:bg-no-repeat transition-all duration-300 hover:bg-right-top hover:bg-[url(./images/service-bg.jpg)] hover:text-white bg-white" onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)} >
+            <div><img className='' src={hovered ? hoverImg : img} /></div>
+            <div className="font-bold text-xl">{title}</div>
+            <div className="text-sm">{content}</div>
+            <div className=""><Link to="/about" className={`${hovered ? 'text-white' : 'text-[#0F70B7]'} transition duration-300 underline`} >En savoir plus</Link></div>
+        </div>
+    )
+}
